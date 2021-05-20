02ea7da6147e9de940306f145ac8e02600df44f5
Travis <146148+rockacola@users.noreply.github.com>
2021-05-20 23:16:09 +1000

    Minor code clean up

--- src/factories/todo-item-factory.ts
+++ src/factories/todo-item-factory.ts
@@ -1,10 +1,15 @@
+import { padStart } from 'lodash'
 import { TodoItemRS } from '../interfaces'
+
+const FIXED_LIST_ID = `l_1`
 
 export class TodoItemFactory {
   static create(title: string): TodoItemRS {
+    const id = padStart(Math.floor(Math.random() * 1000000).toString(), 6, '0')
+
     return {
-      id: `i_${Math.floor(Math.random() * 1000000)}`,
-      listId: 'l_1', // TODO: manage magic string
+      id: `i_${id}`,
+      listId: FIXED_LIST_ID,
       title: title,
       completedAt: -1,
       createdAt: Math.floor(Date.now() / 1000),
